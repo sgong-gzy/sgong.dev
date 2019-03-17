86120afe68922a528e9650cbd676a0ee21c900c6
sgong <shirley.gong90@gmail.com>
2019-03-16 21:00:53 -0700

    wrong place for icon

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,8 +10,6 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-import favicon from '../images/avatar_round.png'
-
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
     graphql`
@@ -80,7 +78,6 @@ function SEO({ description, lang, meta, keywords, title }) {
         )
         .concat(meta)}
         link={[
-          { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
           { rel: 'stylesheet', href: "https://use.fontawesome.com/releases/v5.7.2/css/all.css", integrity: "sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr",  crossorigin: "anonymous"}
         ]}
     />
